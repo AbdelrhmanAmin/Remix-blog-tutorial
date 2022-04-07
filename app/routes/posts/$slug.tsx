302b3f2bb4fd32409725110a52eb1e01ed79d2bd
Abdo Amin <abdo.amin9991@gmail.com>
2022-04-07 12:23:25 +0200

    add markdown!

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,3 +1,4 @@
+import { marked } from "marked";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
@@ -5,22 +6,25 @@ import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
 import type { Post } from "~/models/post.server";
 
-type LoaderData = { post: Post };
+type LoaderData = { post: Post; html: string };
+
 export const loader: LoaderFunction = async ({ params }) => {
   // invariant will throw error if value is false, won't throw if value is true...Cool!
   invariant(params.slug, `params.slug is required`);
   const post = await getPost(params.slug);
   invariant(post, `Post not found: ${params.slug}`);
-  return json<LoaderData>({ post });
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData();
+  const { post, html } = useLoaderData() as LoaderData;
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {post}
+        Some Post: {post.title}
       </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
