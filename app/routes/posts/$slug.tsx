15b7028b4f573736565c28a588f7913dfc55df57
Abdo Amin <abdo.amin9991@gmail.com>
2022-04-07 12:09:56 +0200

    read slug from url params

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,7 +1,21 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+interface IParams {
+  params: { slug: string };
+}
+
+export const loader = async ({ params: { slug } }: IParams) => {
+  return json({ slug });
+};
+
 export default function PostSlug() {
+  const { slug } = useLoaderData();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">Some Post</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">
+        Some Post: {slug}
+      </h1>
     </main>
   );
 }
