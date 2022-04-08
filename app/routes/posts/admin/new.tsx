d40728cf7c46cf891bc8d8a99bfa629b0602d6e7
Abdo Amin <abdo.amin9991@gmail.com>
2022-04-08 10:21:55 +0200

    rm

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -13,9 +13,6 @@ type ActionData =
   | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
-  // TODO: remove me
-  await new Promise((res) => setTimeout(res, 1000));
-
   const formData = await request.formData();
 
   const title = formData.get("title");
